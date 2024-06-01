5c284e7f68b5f16a968ec07d5a8d1a54b910a9b8
Aayusha Ghimire <143613451+AayushaGhimire24@users.noreply.github.com>
2024-06-01 18:31:12 +0545

    Add files via upload

---
+++ Database6delete.php
@@ -0,0 +1,10 @@
+<?php
+$id=$_GET['id'];
+//connect to database
+$conn=mysqli_connect('localhost','root','','myDB');
+//query to delete record from tbl_category with id
+$sql="delete from myGuests where id=$id";
+//execute query
+mysqli_query($conn,$sql);
+//redirect to listing page
+header('location:display');
