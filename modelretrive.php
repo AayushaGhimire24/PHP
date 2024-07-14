1d643221d1e98c1ac9d67cad7582ae5e01a3526d
Aayusha Ghimire <143613451+AayushaGhimire24@users.noreply.github.com>
2024-07-14 18:03:21 +0545

    Add files via upload

---
+++ modelretrive.php
@@ -0,0 +1,31 @@
+<?php
+// Database connection parameters
+$servername = "localhost";
+$username = "root";
+$password = "wxyz";
+$database = "customer";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// SQL query to retrieve data from the table
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"]. " - Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+// Close the connection
+$conn->close();
+?>
